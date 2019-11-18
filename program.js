e1688005d209450ae7001bcf5d3481799290b28e
Thomas Norris <tom.norris135@gmail.com>
2019-11-18 10:47:03 -0500

    assistant works

--- program.js
+++ program.js
@@ -1,12 +1,61 @@
 (function() {
-    const ASSISTANT_CONFIG = {
+    var _path = require('path');
+    var _readline = require('readline');
+    var _assistant = require('google-assistant');
+
+    var _config = {
         auth: {
-            keyFilePath: _path.resolve(__dirname, 'api-key.json'),
-            savedTokensPath: _path.resolve(__dirname, 'saved-tokens.json')
+            // OAuth2 client_secret_*.json downloaded from Google Actions Console
+            keyFilePath: _path.resolve(__dirname, 'client_secret.json'),
+            // Saved tokens (will be created if it doesn't exist)
+            savedTokensPath:  _path.resolve(__dirname, 'client_tokens.json')
+        },
+        conversation: {
+            lang: 'en-US'
         }
     };
 
-    var _path = require('path');
-    var _assistant = new require('google-assistant')(ASSISTANT_CONFIG.auth);
+    _assistant = new _assistant(_config.auth)
+        .on('ready', promptForInput)
+        .on('error', (err) => {
+            console.log('Assistant Error: ' + err);
+        });
 
+    function startConversation(conversation) {
+        conversation
+            .on('response', (text) => {
+                console.log('Assistant Response:', text)
+            })
+
+            // once the conversation is ended, see if we need to follow up
+            .on('ended', (error, continueConversation) => {
+                if (error) {
+                    console.log('Conversation Ended Error:', error);
+                } else if (continueConversation) {
+                    promptForInput();
+                } else {
+                    console.log('Conversation Complete');
+                    conversation.end();
+                }
+            })
+            // catch any errors
+            .on('error', (error) => {
+                console.log('Conversation Error:', error);
+            });
+    }
+
+    function promptForInput() {
+        var rl = _readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        rl.question('Type your request: ', (request) => {
+            // start the conversation
+            _config.conversation.textQuery = request;
+            _assistant.start(_config.conversation, startConversation);
+
+            rl.close();
+        });
+    }
 })();
