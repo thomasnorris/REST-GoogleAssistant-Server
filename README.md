0cac38f3ff859746a5ad39d0c14ebae0763048fa
Thomas Norris <tom.norris135@gmail.com>
2019-11-19 13:20:43 -0500

    Scanning Functionality (#1)
    * scanning started
    
    * scanning works; need to see if it cripples the internet though
    
    * comments
    
    * constants simplification
    
    * more consolidation
    
    * added states
    
    * package modification for repo rename
    
    * update readme
    
    * update packages; console logging
    
    * enable should have been disable
    
    * better interval handling to avoid race conditions
    
    * tabbing
    
    * remove states
    
    * removed states again
    
    * removed states again
    
    * added init parameter

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-## Installation
-- Follow [these](https://developers.google.com/assistant/sdk/guides/service/python/embed/config-dev-project-and-account) instructions to configure a Developer Project.
+## Prerequisites
+### NPM and Node.js
+- Both of these are required and can be downloaded [here](https://github.com/nodesource/distributions).
+    - If using a Raspberry Pi Zero W, follow [these](https://www.thepolyglotdeveloper.com/2018/03/install-nodejs-raspberry-pi-zero-w-nodesource/) instructions instead.
+### Geeni
+- Geeni is connected to the Google Assistant.
+- The cameras are named the same as in `program.js`.
+    - This can be done through the Google Home app or the Geeni app.
+### Google Assistant
+- Follow [these](https://developers.google.com/assistant/sdk/guides/service/python/embed/config-dev-project-and-account) instructions to configure a Google Developer Project.
     - Do not download credentials from the OAuth2 Consent screen.
 - Follow [these](https://developers.google.com/assistant/sdk/guides/service/python/embed/register-device)
  instructions to register the device model.
-    - Anything can be used for the model settings.
-    - No traits need to be specified.
-    - Download __OAuth 2.0__ credentials when prompted. Rename to `client_secret.json` and place in the root directory.
-- Run `program.js` from the command line.
-    - If `client_tokens.json` does not exist, a browser window will open and a token will be generated. This token will be saved into `client_tokens.json`, so this step only happens once.
+    - Anything can be used for the *model settings*.
+    - No *traits* need to be specified.
+    - Download __OAuth 2.0__ credentials when prompted and rename to `client_secret.json`.
+## Installation
+- Everything in the __Prerequisites__ section must be done first!
+- After cloning:
+    - Place `client_secret.json` downloaded earlier in the root of this repository.
+    - Run `npm install` to install packages.
+    - Run `node program.js` to start.
+- Optionally, add the following line to `/etc/rc.local` for auto startup:
+    - `sudo node "/PATH/TO/REPO/program.js"`.
--- program.js
+++ program.js
@@ -1,62 +1,92 @@
 (function() {
-    var _path = require('path');
-    var _readline = require('readline');
-    var _assistant = require('google-assistant');
+    const CLIENT_SECTET_FILE = 'client_secret.json';
+    const CLIENT_TOKENS_FILE = 'client_tokens.json';
+    const CAM_1 = 'Doggo Cam';
+    const CAM_2 = 'People (Ellie) Cam';
+    const ENABLE_MD = 'Enable motion detection';
+    const DISABLE_MD = 'Disable motion detection';
+    // MACs should be cupper case
+    const MACS = ['CC:C0:79:F1:8F:47', 'CC:C0:79:83:5B:18'];
+    const SCAN_INTERVALS = {
+        HOME: '5000',
+        AWAY: '2000'
+    };
 
+    var _path = require('path');
+    var _scan = require('local-devices');
+    var _assistant = require('google-assistant');
     var _assistantConfig = {
         auth: {
-            // OAuth2 client_secret_*.json downloaded from Google Actions Console
-            keyFilePath: _path.resolve(__dirname, 'client_secret.json'),
-            // Saved tokens (will be created if it doesn't exist)
-            savedTokensPath:  _path.resolve(__dirname, 'client_tokens.json')
+            // OAuth2 client_secret_*.json downloaded from Google Actions Console and renamed
+            keyFilePath: _path.resolve(__dirname, CLIENT_SECTET_FILE),
+            // Saved tokens file (will be created if it doesn't exist)
+            savedTokensPath: _path.resolve(__dirname, CLIENT_TOKENS_FILE)
         },
         conversation: {
             lang: 'en-US'
         }
     };
 
-    // start the assistant
+    var _scanIntervalMs = SCAN_INTERVALS.AWAY;
+    var _away = true;
+
+    // start the assistant and scanning
     _assistant = new _assistant(_assistantConfig.auth)
-        .on('ready', promptForInput)
+        .on('ready', () => {
+            startScanning(true);
+        })
         .on('error', (err) => {
             console.log('Assistant Error: ' + err);
         });
 
-    function startConversation(conversation) {
-        conversation
-            .on('response', (text) => {
-                console.log('Assistant Response:', text)
-            })
+    function startScanning(init = false) {
+        _away ? console.log('Away') : console.log('Home');
 
-            // once the conversation is ended, see if we need to follow up
-            .on('ended', (error, continueConversation) => {
-                if (error) {
-                    console.log('Conversation Ended Error:', error);
-                } else if (continueConversation) {
-                    promptForInput();
-                } else {
-                    console.log('Conversation Complete');
-                    conversation.end();
-                }
-            })
-            // catch any errors
-            .on('error', (error) => {
-                console.log('Conversation Error:', error);
+        _scan().then((devices) => {
+            console.log('Scanned', devices.length, 'devices.');
+            var match = devices.some((device) => {
+                return MACS.includes(device.mac.toUpperCase());
             });
+
+            if (match && _away) {
+                // someone just came home
+                sendCommand(DISABLE_MD + ' on ' + CAM_1);
+                sendCommand(DISABLE_MD + ' on ' + CAM_2);
+                _scanIntervalMs = SCAN_INTERVALS.HOME;
+                _away = false;
+            } else if (init || (!match && !_away)) {
+                // everyone just left
+                sendCommand(ENABLE_MD + ' on ' + CAM_1);
+                sendCommand(ENABLE_MD + ' on ' + CAM_2);
+                _scanIntervalMs = SCAN_INTERVALS.AWAY;
+                _away = true;
+            }
+
+            setTimeout(() => {
+                startScanning();
+            }, _scanIntervalMs);
+        });
     }
 
-    function promptForInput() {
-        var rl = _readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-
-        rl.question('Type your request: ', (request) => {
-            // start the conversation
-            _assistantConfig.conversation.textQuery = request;
-            _assistant.start(_assistantConfig.conversation, startConversation);
-
-            rl.close();
+    // log to a file maybe?
+    function sendCommand(command) {
+        _assistantConfig.conversation.textQuery = command;
+        _assistant.start(_assistantConfig.conversation, (conversation) => {
+            conversation
+                .on('response', (text) => {
+                    console.log('Assistant Response:', text)
+                })
+                .on('ended', (error) => {
+                    if (error) {
+                        console.log('Conversation Ended Error:', error);
+                    } else {
+                        console.log('Conversation Complete');
+                        //conversation.end();
+                    }
+                })
+                .on('error', (error) => {
+                    console.log('Conversation Error:', error);
+                });
         });
     }
 })();
